5167f397a515c6c64c582152841ba96ed05adcf9
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-04-20 19:55:30 +0530

    Time: 143 ms (50.05%), Space: 67.6 MB (91.52%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -6,7 +6,7 @@ public:
         
         for(int i=0;i<n;i++){
             sum += nums[i];
-            mxm = max(sum,mxm);
+            mxm = max(sum,mxm); //checks wheher next number is greater or max sumis greater
             
             if(sum<0) 
                 sum = 0;
@@ -15,3 +15,4 @@ public:
         return mxm;
     }
 };
+//this question is based on kadane algo revise once
