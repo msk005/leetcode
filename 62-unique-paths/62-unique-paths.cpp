5fad5f27f97a5f00445d001496bbb888be934e7a
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-04-22 20:33:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (91.03%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
    
     
-    int t[101][101]={};
     
     
-    int uniquePaths(int m, int n, int i=0,int j=0) {
+    
+    int uniquePaths(int m, int n) {
+        int N=m+n-2;   //no of right and bottom moves we need
+        int R=m-1;     ///n-1 is also right, number of bottom moves
+        double ans=1;
         
-        if(i>=n||j>=m)
-            return t[i][j]=0;
-        if(i==(n-1)&&j==(m-1))
-           return t[i][j]=1;
-          
-            if(t[i][j])
-                  return t[i][j];
-               else
-                   return t[i][j]=uniquePaths(m,n,i+1,j)+uniquePaths(m,n,i,j+1)  ;
-          
+        for(int i=1;i<=R;i++)
+        {
+            ans=ans*(N-R+i)/i;
+        }
+        return (int)ans;
     }
 };
