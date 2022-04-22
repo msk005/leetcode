0bc7efac563ade4f5a88bb50a5b28f111d39c6fe
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-04-23 01:49:23 +0530

    Time: 132 ms (51.39%), Space: 31.7 MB (21.38%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -4,19 +4,26 @@ public:
      if(nums.size()==0)
          return 0;
         int c=1;
-        int mxm=INT_MIN;
-        sort(nums.begin(),nums.end());
-        for(int i=1;i<nums.size();i++)
-        { if(nums[i]!=nums[i-1])
-           if(nums[i]==1+nums[i-1])
-               c++;
-            else
-            {
-                mxm=max(c,mxm);
-                 c=1;
-            }
+        int mx=INT_MIN;
+    set<int>s;
+        for(auto x:nums)
+            s.insert(x);
+        
+        for(auto x: s)
+        {
+           if(s.find(x-1)==s.end())
+           {
+               int currN=x;
+               c=1;
+            
+              while(s.find(currN+1)!=s.end())
+              {
+                    currN+=1;
+                     c+=1;
+               }
+            mx=max(mx,c);
+           }
         }
-        return max(mxm,c);
-                
+       return mx;         
     }
 };
