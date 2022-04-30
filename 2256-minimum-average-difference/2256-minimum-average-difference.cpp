ab65750c9f57170dbb0df54330c119d57a1c9b1f
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-04-30 21:55:23 +0530

    Time: 146 ms (33.33%), Space: 93.6 MB (100.00%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.cpp
+++ 2256-minimum-average-difference/2256-minimum-average-difference.cpp
@@ -4,15 +4,16 @@ public:
        int mnm=INT_MAX;
        int n=nums.size();
        int idx;
-        vector<int>v1;
-        vector<int>v2;
-       long long int s1=0;
-        long long int s2=0;
+        vector<int>v1,v2;
+        
+       long long int s1=0,s2=0;
+        
        for(int i=0;i<n;i++)
        {
            s1+=nums[i];
            v1.push_back(s1/(i+1));
        }
+        
         int i=1;
         v2.push_back(0);
         for(int j=nums.size()-1;j>=1;j--)
@@ -26,19 +27,15 @@ public:
         reverse(v2.begin(),v2.end());
         vector<int>ans;
         for(int k=0;k<n;k++)
-        {
-            ans.push_back(abs(v1[k]-v2[k]));
-            
-        }
-        
-        for(int m=0;m<ans.size();m++)
-        { 
-            if(ans[m]<mnm)
+        {  
+            if((abs(v1[k]-v2[k]))<mnm)
             {
-                mnm=ans[m];
-                idx=m;
+                mnm=(abs(v1[k]-v2[k]));
+                idx=k;
             }
         }
+        
+        
         
 //         for(int x:v2)
 //             cout<<x;
