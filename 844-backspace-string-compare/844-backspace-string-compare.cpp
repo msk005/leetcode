7477d8c8b1dd1aece85ba5c8a50a69344afb7f51
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-05-04 01:53:48 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (35.11%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,47 +1,28 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-       stack <char> stk1;
-        stack<char>stk2;
+       string a="",b="";
         
-         string str1,str2; 
-            
-         for(int i = 0; i < s.length(); i++){
-                 
-             if(s[i] != '#')
-                         stk1.push(s[i]);
-             
-                 else if(!stk1.empty()&&s[i] == '#')
-                         stk1.pop();
-                 
-                 
-                 
-         }
-         
-         for(int i = 0; i < t.length(); i++){
-                 if(t[i] != '#')
-                         stk2.push(t[i]);
-                else if(t[i] == '#' && !stk2.empty())
-                         stk2.pop();
-                 
-                  
-                 
-         }
-            
-         while(!stk1.empty()){
-                 
-                 str1.push_back(stk1.top());
-                 stk1.pop();
-                 
-         }
-            
-         while(!stk2.empty()){
-                 
-                 str2.push_back(stk2.top());
-                 stk2.pop();
-                 
-         }      
-            
-         return str1 == str2;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='#')
+            {if(a.length()!=0)
+                a.pop_back();
+            }
+              
+            else
+                a+=s[i];
+        }
+        for(int i=0;i<t.length();i++)
+        {
+            if(t[i]=='#')
+            {if(b.length()!=0)
+                b.pop_back();}
+              
+            else
+                b+=t[i];
+        }
+        
+        return a==b;
     }
 };
