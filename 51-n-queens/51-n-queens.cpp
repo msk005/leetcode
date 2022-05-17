492ab956e3f23ed1a2f96ab2b9bd915b357407ac
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-05-18 01:08:54 +0530

    Time: 9 ms (58.82%), Space: 7.6 MB (43.01%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    vector<vector<string>>ans;
+    void solve(int col,vector<string> &board,vector<int>&leftRow, vector<int>&upDiag, vector<int>&loDiag,int n)
+    {
+        if(col==n)
+        {
+            ans.push_back(board);
+            return;
+        }
+        
+        for(int row=0;row<n;row++)
+        {
+          
+            if(!leftRow[row] and !loDiag[row+col] and !upDiag[n-1+col-row])
+            {
+                
+                board[row][col]='Q';
+                
+                leftRow[row]=1;
+                loDiag[row+col]=1;
+                upDiag[n-1+col-row]=1;
+                
+                solve(col+1,board,leftRow,upDiag,loDiag,n);
+                
+                board[row][col]='.';
+                
+               leftRow[row]=0;
+                loDiag[row+col]=0;
+                upDiag[n-1+col-row]=0;
+            }
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string>board(n);
+        string s(n,'.');
+        
+        for(int i=0;i<n;i++)
+        {
+            board[i]=s;
+        }
+        
+        vector<int>leftRow(n,0),upDiag(2*n-1,0),loDiag(2*n-1,0);
+        
+        solve(0,board,leftRow,upDiag,loDiag,n);
+        return ans;
+    }
+};
