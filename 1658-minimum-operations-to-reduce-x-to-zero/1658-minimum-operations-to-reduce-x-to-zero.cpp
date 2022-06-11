806c1c8e60057e28bafb77b76574ecf2e97ad574
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-06-12 00:38:35 +0530

    Time: 170 ms (95.17%), Space: 98.6 MB (71.00%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+       
+        int n = nums.size(), s = 0, ans = INT_MAX;
+        
+        for(int i = 0; i < n ; i++) s += nums[i];
+        
+        if(s < x) return -1;
+        
+        int st = 0, end = 0, windowSum = 0;
+        
+        while(end < n){            
+            windowSum += nums[end];            
+            while(windowSum > (s - x)){
+                windowSum -= nums[st]; //Reducing window size
+                st++;
+            }            
+            if(windowSum == s - x){
+                ans = min(ans, n - (end - st + 1));
+            }            
+            end++;            
+        }
+		
+        return (ans == INT_MAX ? -1 : ans);    
+    }
+};
