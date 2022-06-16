74aad52d2edd2dea2fa52aa10b5fe4a2e09a2f2b
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-06-16 21:50:23 +0530

    Time: 278 ms (91.41%), Space: 99.7 MB (67.69%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -8,23 +8,22 @@ public:
             for(int i=0;i<spells.size();i++)
             {
                 int l=0;
-                 int h=potions.size();
+                 int h=potions.size()-1;
                 
                 
-                while(h>l)
+                while(l<=h)
                 {
                     int mid=l+(h-l)/2;
-                    long long spell=spells[i];
-                    long long potion=potions[mid];
-                    long long need=spell*potion;
-                    if(need>=success)
+                    long long spell=spells[i],potion=potions[mid];
+                    
+                    if(spell*potion>=success)
                     {
-                        h=mid;
+                        h=mid-1;
                     }
                     else
                         l=mid+1;
                 }
-                vec.push_back(potions.size()-h);
+                vec.push_back(potions.size()-h-1);
 
           }
         return vec;
