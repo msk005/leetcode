55b8394b55e338b2a6b35d548eca828c863fc468
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-06-17 23:02:43 +0530

    Time: 17 ms (49.00%), Space: 12.4 MB (71.44%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.cpp
@@ -1,34 +1,20 @@
 class Solution {
 public:
-    bool anagram(string a,string b)
-    {
-        if(a.length()!=b.length())
-            return false;
-        unordered_map<char,int>um;
-        for(int i=0;i<a.length();i++)
-        {
-            um[a[i]]++;
-            um[b[i]]--;
-            
-            
-        }
-        
-        for (auto x:  um)
-               if (x.second)
-            return false;
-    return true;
-    }
+    
     vector<string> removeAnagrams(vector<string>& words) {
-        vector<string> ans;
-       int n= words.size();
-     for(int i=0;i<n;)
-     {
-        int j=i+1;
-        while( j<n and anagram(words[i],words[j]) ) 
-            j++;
-        ans.push_back(words[i]);
-        i=j;
-     }
-    return ans;
+       vector<string>ans;
+     ans.push_back(words[0]);
+    for (int i = 1; i<words.size(); i++) 
+    { 
+        string a=words[i];
+        string b=ans[ans.size()-1];
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        if(a!=b)
+        {
+            ans.push_back(words[i]);
+        }
+    }
+        return ans;
     }
 };
