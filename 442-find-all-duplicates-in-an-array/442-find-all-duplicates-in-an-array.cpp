09b4df7f9153664263daabeb2a0bdc751062912a
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-06-18 18:07:43 +0530

    Time: 52 ms (97.36%), Space: 33.7 MB (48.87%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-        vector<int>v;
-        unordered_map<int,int>um;
+//         vector<int>v;
+//         unordered_map<int,int>um;
         
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             if(um.find(nums[i])!=um.end())
+//             {
+//                v.push_back(nums[i]); 
+//             }
+//             else
+//                 um[nums[i]]++;
+//         }
+        
+//         return v;
+        
+        
+//         without space
+        
+    // we will make elements negative and check if its alrready neagtive meanis it is duplicate otherwise make it negaitve
+        
+        vector<int>v;
         for(int i=0;i<nums.size();i++)
         {
-            if(um.find(nums[i])!=um.end())
+            int index=abs(nums[i])-1;
+            if(nums[index]<0)   //mtlb phle he ye element aa chuka hai
             {
-               v.push_back(nums[i]); 
+                v.push_back(abs(nums[i]));
             }
+            
             else
-                um[nums[i]]++;
+                nums[index]=-nums[index];
         }
-        
         return v;
+        
     }
 };
