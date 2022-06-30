b57b35a1cd24a708d2636cfe86a86f4550f84259
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-06-30 22:48:54 +0530

    Time: 18 ms (42.75%), Space: 10.9 MB (15.75%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -7,11 +7,11 @@ public:
             return 0;
         sort(nums.begin(),nums.end(),greater<int>());
         int p;
-        
-        if(n%2!=0)
+//         
+        // if(n%2!=0)
             p=nums[n/2];
-        else
-            p=nums[(n+1)/2];
+        // else
+        //     p=nums[(n+1)/2];
         int ans=0;
         for(auto x:nums)
             ans+=abs(x-p);
