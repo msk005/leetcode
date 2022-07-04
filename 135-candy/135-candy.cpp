77ef7d247fad578e0bacef210a5ef8dbc83f46ea
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-07-05 01:09:32 +0530

    Time: 34 ms (49.67%), Space: 17.9 MB (41.00%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -7,7 +7,8 @@ public:
         
     vector<int>lefttR(n,1);
         
-        vector<int>righttL(n,1);
+        // vector<int>righttL(n,1);
+        
         int ans=0;
         for(int i=1;i<n;i++)
         {
@@ -16,25 +17,34 @@ public:
                                                       // hain usse 1 jayda dedo
         }
         
+        // for(int i=n-2;i>=0;i--)
+        // {
+        //     if(ratings[i]>ratings[i+1])    //agar peeche ka element less hai to usse ek candy jayada dedo
+        //       righttL[i]=righttL[i+1]+1;
+        // }
+        
+        
         for(int i=n-2;i>=0;i--)
         {
-            if(ratings[i]>ratings[i+1])    //agar peeche ka element less hai to usse ek candy jayada dedo
-              righttL[i]=righttL[i+1]+1;
+            if(ratings[i]>ratings[i+1])
+                lefttR[i]= max(lefttR[i],lefttR[i+1]+1);           
         }
         
-     for(int i=0;i<n;i++)
-     {
-         ans+=max(righttL[i],lefttR[i]);
-     }
+     // for(int i=0;i<n;i++)
+     // {
+     //     ans+=max(righttL[i],lefttR[i]);
+     // }
         
-       for(auto x:righttL)
-           cout<<x<<endl;
+//        for(auto x:righttL)
+//            cout<<x<<endl;
         
-        cout<<endl<<endl;
+//         cout<<endl<<endl;
+        
+//         for(auto x:lefttR)
+//             cout<<x<<endl;
         
         for(auto x:lefttR)
-            cout<<x<<endl;
-        
+            ans+=x;
         return ans;
         
         
