322e2a8c3bf98d18f08d2dd57b201d345e915b11
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-07-17 22:47:12 +0530

    Time: 2619 ms (5.13%), Space: 10.3 MB (75.64%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
-  int mod = (int)(1e9 + 7);
-    
-    int f(int n, int k, vector<vector<int>>& t){
-       
-        if(k<=0) return k==0;
-        if(n<=0) return 0;
-        
-       
-        if(t[n][k]!=-1) 
-            return t[n][k];
-        
-        
-        t[n][k] = (f(n-1, k, t) + f(n, k-1, t))%mod;
-        
-        return t[n][k] = (t[n][k] - f(n-1, k-n, t) + mod)%mod;
-    }
-    int kInversePairs(int n, int k) {
-        
-        vector<vector<int>> t(n+1,vector<int>(k+1,-1));
-        return f(n,k,t);
-    }
+   int kInversePairs(int n, int k) {
+    int dp[1001][1001] = { 1 };
+    for (int N = 1; N <= n; ++N)
+        for (int K = 0; K <= k; ++K)
+            for (int i = 0; i <= min(K, N - 1); ++i) 
+                dp[N][K] = (dp[N][K] + dp[N - 1][K - i]) % 1000000007;
+    return dp[n][k];
+}
 };
