3e2f0a26bd3d3c3880bd9e87a8e511fb9b1de493
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-07-21 00:24:35 +0530

    Time: 172 ms (95.28%), Space: 37.9 MB (87.39%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,10 +1,44 @@
 class Solution {
 public:
     unordered_map<string,int>um;
-    bool helper(string s,string t)
-    {
-         int n=s.length();
-         int m=t.length();
+//     bool helper(string s,string t)
+//     {
+//          int n=s.length();
+//          int m=t.length();
+//          int c=0;
+//         for(int i=0,j=0;i<n and j<m ;)
+//         {
+//             if(s[i]==t[j])
+//             {
+//                 c++;
+//                 i++,j++;}
+//             else
+//                 j++;
+//         }
+        
+//         if(c==n)
+//             return true;
+        
+        
+//         return false;
+//     }
+        
+        
+        
+        
+    
+    int numMatchingSubseq(string s, vector<string>& words) {
+        int count=0;
+        int n=s.length();
+        for(int i=0;i<words.size();i++)
+        {
+            um[words[i]]++;
+        }
+        
+        for(auto x:um)
+        {    
+            string t=x.first;
+           int m=t.length();
          int c=0;
         for(int i=0,j=0;i<n and j<m ;)
         {
@@ -13,32 +47,11 @@ public:
                 c++;
                 i++,j++;}
             else
-                j++;
+                i++;
         }
         
-        if(c==n)
-            return true;
-        
-        
-        return false;
-    }
-        
-        
-        
-        
-    
-    int numMatchingSubseq(string s, vector<string>& words) {
-        int count=0;
-        for(int i=0;i<words.size();i++)
-        {
-            um[words[i]]++;
-        }
-        
-        for(auto x:um)
-        {
-            string str=x.first;
-            if(helper(str,s))
-                count+=x.second;
+        if(c==m)
+                count+=x.second;  //we are using thid if string is repaeted so that we dont have to call function again and agin
         }
         return count;
     }
