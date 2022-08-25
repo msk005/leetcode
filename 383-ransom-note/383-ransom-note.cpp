3a859511e9e87184f759e83af80bf138ed10dff2
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-08-25 21:44:07 +0530

    Time: 45 ms (19.73%), Space: 8.8 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-          unordered_map<char, int>um(26);
-        for (int i = 0; i < magazine.length(); i++)
-            um[magazine[i]]++;
-        for (int i = 0; i < ransomNote.length(); i++)
-            if (--um[ransomNote[i]] < 0)
+        unordered_map<char,int>um;
+        for(auto x:ransomNote)
+        {
+            um[x]++;
+        }
+        
+        for(auto x:magazine)
+        {
+           if(um.find(x)!=um.end())
+               um[x]--;
+        }
+        
+        for(auto x:um)
+        {
+            if(x.second>0)
                 return false;
+        }
+        
         return true;
     }
 };
