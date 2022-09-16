4a3f4e755cbe9970e67cd5f6fc70bd52a4adb0ef
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-09-17 00:34:07 +0530

    Time: 642 ms (60.75%), Space: 54.6 MB (98.40%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,32 +1,36 @@
-class Solution
-{
-    public:
-
-        int dp[1001][1001];
-    int help(int i, vector<int> &M, vector<int> &nums, int s)
+class Solution {
+public:
+    
+   int t[1001][1001];
+    int helper(vector<int>& nums, vector<int>& multipliers, int i,int s )
     {
-
-        if (i == M.size())
+        
+        
+        if(i==multipliers.size())
             return 0;
-
-        if (dp[i][s] != INT_MIN)
-            return dp[i][s];
-
-        int e = nums.size() - (i - s) - 1;
-
-        int op1 = M[i] *nums[s] + help(i + 1, M, nums, s + 1);
-        int op2 = M[i] *nums[e] + help(i + 1, M, nums, s);
-
-        return dp[i][s] = max(op1, op2);
+        
+        if(t[i][s]!=INT_MIN)
+            return t[i][s];
+        
+        int e=(nums.size()-1)-(i-s);
+        
+        int res1=multipliers[i]*nums[s]+helper( nums, multipliers, i+1,s+1);
+        
+        int res2=multipliers[i]*nums[e]+helper( nums, multipliers, i+1,s);
+        
+       return t[i][s]=max(res1,res2);
+        
     }
-    int maximumScore(vector<int> &nums, vector<int> &M)
-    {
-
-      for(int i=0;i<1001;i++)
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {
+        
+        for(int i=0;i<1001;i++)
       {
           for(int j=0;j<1001;j++)
-              dp[i][j]=INT_MIN;
+              t[i][j]=INT_MIN;
       }
-        return help(0, M, nums, 0);
+        
+        return helper(nums,multipliers,0,0);
+       
+            
     }
 };
