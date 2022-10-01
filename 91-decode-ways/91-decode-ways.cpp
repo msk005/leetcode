aa8d961301831a0e7cec9ddd9629e78c67aa25f2
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-10-02 01:35:03 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (75.56%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int t[101];
+    int helper(int i,string &s)
+    {
+        if(i>=s.size())
+            return 1;
+        if(t[i]!=-1)
+            return t[i];
+        
+        int ans=0;
+        int op1=s[i]-'0';
+        int op2=0;
+        if(i<s.size()-1)  //iska mtlb 2 character hai string m atleast
+        {
+            // if()
+            // string str=s.substr(i,2);
+             op2=op1*10+s[i+1]-'0';
+            
+        }
+        if(op1>0)
+        {
+            ans+=helper(i+1,s);
+        }
+        if(op1!=0 and op2>0 and op2<=26)
+        {
+            ans+=helper(i+2,s);
+        }
+        return t[i]=ans;
+        
+    }
+    int numDecodings(string s) {
+     memset(t,-1,sizeof(t));
+        return helper(0,s);
+    }
+};
