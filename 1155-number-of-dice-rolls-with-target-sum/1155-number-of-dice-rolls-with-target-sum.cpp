01d9bf7c7f349f3670d8cd84a06489ee5db19ecd
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-10-03 00:38:58 +0530

    Time: 35 ms (69.33%), Space: 8.3 MB (59.18%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+//     #define MOD 100000007;
+//     int t[31][1001];
+//     int solve(int n,int k,int target)
+//     {
+//         if(n==0)
+//         {
+            
+//         if(target==0)
+//             return 1;
+            
+           
+//         }
+//         if (n == 0 || target < 0) return 0;
+//         if(t[n][target]!=0)
+//             return t[n][target];
+//         int c=0;
+//         for(int i=1;i<=k;i++)
+//         {
+//           c+=solve(n-1,k,target-1)%MOD;
+            
+//         }
+        
+//         return t[n][target]=c;
+//     }
+ 
+    
+//     int numRollsToTarget(int n, int k, int target) {
+//         memset(t,0,sizeof(t));
+//        return solve(n,k,target);
+//     }
+// };
+         	int mod=1e9+7;
+	int f(int i,int tar,int k,int n,vector<vector<int>>& dp){
+		if(i==n){
+			if(!tar) return 1;
+			return 0;
+		}    
+		if(dp[i][tar]!=-1) return dp[i][tar];
+		long long p=0;
+		for(int j=1;j<=k;j++){
+			if(tar>=j) p+=f(i+1,tar-j,k,n,dp)%mod;
+		}
+		return dp[i][tar]=p%mod;
+	}
+
+	int numRollsToTarget(int n, int k, int tar) {
+		vector<vector<int>> dp(n,vector<int>(tar+1,-1));
+		return f(0,tar,k,n,dp)%mod;
+	}
+};
