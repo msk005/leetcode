fa8c8e04e3c72ed31c39b2be8504d349264aeb10
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2022-10-07 00:39:12 +0530

    Time: 569 ms (57.93%), Space: 128.9 MB (43.93%) - LeetHub

---
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -0,0 +1,30 @@
+class TimeMap {
+public:
+    unordered_map<string,unordered_map<int,string>>um;
+    TimeMap() {
+        
+    }
+    
+    void set(string key, string value, int timestamp) {
+        um[key][timestamp]=value;
+    }
+    
+    string get(string key, int timestamp) {
+        if(um.find(key)==um.end())
+            return "";
+        for(int i=timestamp;i>0;i--)
+        {
+            if(um[key].find(i)!=um[key].end())
+                return um[key][i];
+        }
+        return "";
+    }
+};
+
+  
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
