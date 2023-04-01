a222dbf730ec023331515ead76aef127e1f3b96b
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2023-04-01 21:44:32 +0530

    Time: 47 ms (17.08%), Space: 27.7 MB (18.34%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+          int h=nums.size()-1;
+        int l=0;
+        int mid;
+        while(l<=h){
+            mid=(l+h)/2;
+        if(nums[mid]==target)
+            return mid;
+         else if
+             (nums[mid]<target)
+         {
+             l=mid+1;
+             
+         }
+            
+           else
+               h=mid-1;
+               
+        }
+        
+      return -1;  
+    }
+};
