16a58273253317d7ecaba328007ee402a4a3728c
Manish Singh Kushwaha <90471325+msk005@users.noreply.github.com>
2023-07-21 23:22:31 +0530

    Time: 8 ms (81.53%), Space: 13.4 MB (99.90%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -2,13 +2,58 @@ class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
         int n= nums.size();
-        int lb = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        if(lb==n or nums[lb]!=target)
-            return {-1,-1};
+//         int lb = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+//         if(lb==n or nums[lb]!=target)
+//             return {-1,-1};
         
-        int ub =upper_bound(nums.begin(),nums.end(),target)-nums.begin();
+//         int ub =upper_bound(nums.begin(),nums.end(),target)-nums.begin();
          
-        return {lb, ub-1};
+//         return {lb, ub-1};
         
+        int h=n-1,l=0;
+        
+        //first
+        int first=-1;
+        
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target)
+            {
+                first=mid;
+                h=mid-1;   //first will be on left side
+                
+            }
+            
+            else if(nums[mid]<target)
+                l=mid+1;
+            
+            
+            else
+                h=mid-1;
+        }
+        
+        //last
+        h=n-1,l=0;
+                int last=-1;
+        
+        while(l<=h)
+        {
+            int mid=l+(h-l)/2;
+            if(nums[mid]==target)
+            {
+                last=mid;
+                l=mid+1;   //last will be in right side
+                
+            }
+            
+            else if(nums[mid]<target)
+                l=mid+1;
+            
+            else
+             h=mid-1;
+        }
+       return {first,last}; 
     }
+    
 };
